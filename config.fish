0ea98deac1b37b2b5adce6fdedc624131ede6dfa
Barnaby Colby <barnaby.colby@gmail.com>
2024-08-14 16:02:34 +0100

    Only load the shell colour theme in fish, not bash

--- config.fish
+++ config.fish
@@ -35,3 +35,8 @@ function rgp
 end
 
 alias load_bash='bash --rcfile ~/dotfiles/shellrc'
+
+if status --is-interactive
+    set BASE16_SHELL "$HOME/.config/base16-shell/"
+    source "$BASE16_SHELL/profile_helper.fish"
+end
